6a8616f4f0c40f304f665d542c7840a0af2a9949
Christian Hackl <chack@live.at>
2016-09-24 12:03:59 +0200

    Initial Commit

---
+++ Guitar Fretboard Scale Generator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Guitar Fretboard Scale Generator
+//
+//  Created by Christian Hackl on 24.09.16.
+//  Copyright © 2016 Christian Hackl. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Guitar Fretboard Scale Generator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Guitar Fretboard Scale Generator
+//
+//  Created by Christian Hackl on 24.09.16.
+//  Copyright © 2016 Christian Hackl. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
