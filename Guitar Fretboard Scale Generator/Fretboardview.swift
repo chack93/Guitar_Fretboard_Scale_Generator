0a13a8633c454c8827c4b3425886e7f2ea12a93f
Christian Hackl <chack@live.at>
2016-09-25 21:32:25 +0200

    ADD scale creation
    ADD first draft of note dot drawing

--- Guitar Fretboard Scale Generator/Fretboardview.swift
+++ Guitar Fretboard Scale Generator/Fretboardview.swift
@@ -29,26 +29,30 @@ struct NoteDot {
 }
 
 @IBDesignable
-class Fretview: NSView {
-    var noteDots = [NoteDot]() {
+class Fretboardview: NSView {
+    /// Frets to be drawn
+    let frets = 16
+    
+    /// Root note will be drawn red
+    var rootNote = "A"
+    
+    /// The scale to be drawn  
+    /// Format: [6 string to 1st (high e][note on 1 fret]
+    /// Notes that are not whithin the cromatic scale won't be displayed.
+    /// Use sharp instead of flat for half notes. Example: C#, D#, ...
+    var notes = [[String]]() {
         didSet {
             self.needsDisplay = true
         }
     }
     
-    /*
-    element.color!.set()
-    element.bezierPath!.fill()
-    element.bezierPath!.stroke()
-    */
+    private var noteDots = [NoteDot]()
     
     override func drawRect(dirtyRect: NSRect) {
         super.drawRect(dirtyRect)
         drawFretboard()
         
-        for dot in noteDots {
-            drawNote(dot)
-        }
+        drawNotes()
     }
     
     func drawFretboard() {
@@ -74,10 +78,9 @@ class Fretview: NSView {
         fretColor.set()
         nut.fill()
         
-        let frets = 16
         fretColor.set()
-        let leftPadding = viewSize.width / CGFloat(frets)
-        for nFret in 1...frets {
+        let leftPadding = viewSize.width / CGFloat(self.frets)
+        for nFret in 1...self.frets {
             let totalLeftPadding = leftPadding * CGFloat(nFret)
             let nFretRect = fretRect.offsetBy(dx: totalLeftPadding, dy: 0).insetBy(dx: 0, dy: paddingY/4)
             let fret = NSBezierPath(roundedRect: nFretRect, xRadius: fretWidth/2, yRadius: fretWidth/2)
@@ -86,7 +89,32 @@ class Fretview: NSView {
         }
     }
     
+    func drawNotes() {
+        let paddingY = self.bounds.width * 0.012
+        let fretDistance = self.bounds.width / CGFloat(self.frets)
+        let dotHeight = CGFloat((self.bounds.height - paddingY) / 6)
+        let allowedNotes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+        
+        for i in 0..<self.notes.count {
+            for j in 0..<self.notes[i].count {
+                // Jump over empty notes
+                if (!allowedNotes.contains(notes[i][j])) {
+                    continue
+                }
+                let leftPadding = fretDistance * CGFloat(j)
+                let dotRect = NSRect(x: leftPadding,
+                                     y: dotHeight * CGFloat(i),
+                                     width: dotHeight,
+                                     height: dotHeight)
+                let isRootNote = notes[i][j] == rootNote
+                let dot = NoteDot.init(size: dotRect, note: notes[i][j], isRootNote: isRootNote)
+                drawNote(dot)
+            }
+        }
+    }
+    
     func drawNote(note: NoteDot) {
+        
         note.dotColor.set()
         note.bezierPath.fill()
         
--- Guitar Fretboard Scale Generator/ViewController.swift
+++ Guitar Fretboard Scale Generator/ViewController.swift
@@ -9,7 +9,12 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    // Outlets
+    @IBOutlet weak var fretboardView: Fretboardview!
+    @IBOutlet weak var keySelection: NSPopUpButton!
+    @IBOutlet weak var scaleSelection: NSPopUpButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +26,75 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
+    
+    @IBAction func changeKey(sender: NSPopUpButton) {
+        let selectedKey = self.keySelection.titleOfSelectedItem!
+        let selectedScaleIndex = self.scaleSelection.indexOfSelectedItem
+        let majorScale = [2, 2, 1, 2, 2, 2, 1]
+        var selectedScale = [Int](count: majorScale.count, repeatedValue: 0)
+        
+        for i in 0 ..< majorScale.count {
+            let shift = (i + selectedScaleIndex) % majorScale.count
+            selectedScale[i] = majorScale[shift]
+        }
+        self.createScaleWithKey(selectedKey.capitalizedString, scale: selectedScale)
+    }
+    
+    @IBAction func changeScale(sender: NSPopUpButton) {
+        let selectedKey = self.keySelection.titleOfSelectedItem!
+        let selectedScaleIndex = self.scaleSelection.indexOfSelectedItem
+        let majorScale = [2, 2, 1, 2, 2, 2, 1]
+        var selectedScale = [Int](count: majorScale.count, repeatedValue: 0)
+        
+        for i in 0 ..< majorScale.count {
+            let shift = (i + selectedScaleIndex) % majorScale.count
+            selectedScale[i] = majorScale[shift]
+        }
+        self.createScaleWithKey(selectedKey.capitalizedString, scale: selectedScale)
+    }
+    
+    func createScaleWithKey(key: String, scale: [Int]) {
+        let frets = fretboardView.frets
+        let notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+        var notesInScale = [String](count: 7, repeatedValue: "-")
+        var resultScale = Array(count: 6, repeatedValue: [String](count: frets, repeatedValue: "-"))
+        // Start at the 1st fret at each string
+        resultScale[0][0] = "F"
+        resultScale[1][0] = "A#"
+        resultScale[2][0] = "D#"
+        resultScale[3][0] = "G#"
+        resultScale[4][0] = "C"
+        resultScale[5][0] = "F"
+        
+        // Add all notes up to 16nd string
+        for i in 0..<resultScale.count {
+            let firstNote = resultScale[i][0]
+            let firstNoteIndex = notes.indexOf(firstNote)!
+            
+            for j in 0..<resultScale[i].count {
+                let indexOfNote = (firstNoteIndex + j) % notes.count
+                resultScale[i][j] = notes[indexOfNote]
+            }
+        }
+        
+        // Get the 7 scale notes
+        var indexFromKeyNote = notes.indexOf(key)!
+        for i in 0..<notesInScale.count {
+            notesInScale[i] = notes[indexFromKeyNote % notes.count]
+            indexFromKeyNote += scale[i]
+        }
+        
+        // Replace all notes that are not whithin the scale notes
+        for i in 0..<resultScale.count {
+            for j in 0..<resultScale[i].count {
+                if (!notesInScale.contains(resultScale[i][j])) {
+                    resultScale[i][j] = "-"
+                }
+            }
+        }
+        
+        fretboardView.rootNote = key
+        fretboardView.notes = resultScale
+    }
 }
 
