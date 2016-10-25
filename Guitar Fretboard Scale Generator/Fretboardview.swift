3972f94392243d5e64f871a100cfe41c356f0cc5
Christian Hackl <chack@live.at>
2016-10-25 22:56:20 +0200

    ADDED fret position field
    FIXED attributes for text drawing

--- Guitar Fretboard Scale Generator/Fretboardview.swift
+++ Guitar Fretboard Scale Generator/Fretboardview.swift
@@ -32,14 +32,26 @@ struct NoteDot {
 class Fretboardview: NSView {
     /// Frets to be drawn
     let frets = 16
-    private var nutWidth:CGFloat = 0.0;
-    private var fretWidth:CGFloat = 0.0;
-    private var strokeWidth:CGFloat = 0.0;
+    private var nutWidth:CGFloat = 0.0
+    private var fretWidth:CGFloat = 0.0
+    private var strokeWidth:CGFloat = 0.0
+    private var fretboardRect:CGRect = CGRect()
     
     /// Root note will be drawn red
     var rootNote = "A"
     
-    /// The scale to be drawn  
+    enum FretPositionMarkers {
+        case dots
+        case digits
+        case romanDigits
+        case none
+    }
+    
+    /// Signs to draw for 3, 5, 7, 9, 12 & 15 position
+    /// Will be set to digits if not defined
+    var fretPositionMarker:FretPositionMarkers = .digits
+    
+    /// The scale to be drawn
     /// Format: [6 string to 1st (high e][note on 1 fret]
     /// Notes that are not whithin the cromatic scale won't be displayed.
     /// Use sharp instead of flat for half notes. Example: C#, D#, ...
@@ -63,28 +75,44 @@ class Fretboardview: NSView {
         let fretboardColor = NSColor(calibratedRed: 174/255, green: 134/255, blue: 108/255, alpha: 1)
         let fretColor = NSColor(calibratedRed: 107/255, green: 117/255, blue: 138/255, alpha: 1)
         let viewSize = self.bounds
+        let fretPositionMarkerHeight = viewSize.height * 0.1
         
         // Nut
         self.nutWidth = viewSize.width * 0.02
-        let nutRect = NSRect(x: 0, y: 0, width: nutWidth, height: viewSize.height)
+        let nutRect = NSRect(x: 0, y: fretPositionMarkerHeight, width: nutWidth, height: viewSize.height - fretPositionMarkerHeight)
         let nutPath = NSBezierPath(roundedRect: nutRect, xRadius: nutWidth/2, yRadius: nutWidth/2)
         
         // Frets
         self.fretWidth = viewSize.width * 0.012
-        let fretRect = NSRect(x: 0, y: 0, width: fretWidth, height: viewSize.height)
+        let fretRect = NSRect(x: 0, y: fretPositionMarkerHeight, width: fretWidth, height: viewSize.height - fretPositionMarkerHeight)
         
         // Fretboard
-        let fbXPadding:CGFloat = nutWidth / 4 // Place left border in center of the nut
-        let fretboardRect = viewSize.insetBy(dx: fbXPadding, dy: 0).offsetBy(dx: fbXPadding, dy: 0).insetBy(dx: self.strokeWidth, dy: strokeWidth)
-        let fretboardPath = NSBezierPath(rect: fretboardRect)
+        let fbLeftPadding:CGFloat = nutWidth / 2 // Place left border in center of the nut
+        self.fretboardRect = CGRect(x: 
+            fbLeftPadding, 
+            y: fretPositionMarkerHeight, 
+            width: viewSize.width - fbLeftPadding, 
+            height: viewSize.height - fretPositionMarkerHeight).insetBy(dx: self.strokeWidth, 
+            dy: self.strokeWidth)
         
-        
+        // Paint fret position marker rect
+        let fretPositionMarkerRect = CGRect(x: 0, y: 0, width: viewSize.width, height: fretPositionMarkerHeight)
+        let fretPosPath = NSBezierPath(roundedRect: fretPositionMarkerRect, xRadius: 5.0, yRadius: 5.0)
         NSColor.black.set()
+        fretPosPath.fill()
+        
+        // create fret position markers
+        self.createMarkers(markerRect: fretPositionMarkerRect, selectedMarkers: self.fretPositionMarker)
+        
+        // Paint fretboard
+        let fretboardPath = NSBezierPath(rect: self.fretboardRect)
         fretboardPath.lineWidth = strokeWidth
+        NSColor.black.set()
         fretboardPath.stroke()
         fretboardColor.set()
         fretboardPath.fill()
         
+        // Paint nut
         fretColor.set()
         nutPath.fill()
         
@@ -98,9 +126,83 @@ class Fretboardview: NSView {
         }
     }
     
+    func createMarkers(markerRect: CGRect, selectedMarkers: FretPositionMarkers) {
+        // create rect's for each position
+        
+        let markerWidth = markerRect.height * 0.8
+        let bottomPadding = (markerRect.height - markerWidth) / 2
+        let x = markerRect.width / CGFloat(self.frets)
+        let posRightPadding = markerWidth + (x - markerWidth - self.fretWidth) / 2
+        
+        let pos1 = CGRect.init(x: x * 3 - posRightPadding,
+                               y: bottomPadding,
+                               width: markerWidth,
+                               height: markerWidth)
+        let pos2 = CGRect.init(x: x * 5 - posRightPadding,
+                               y: bottomPadding,
+                               width: markerWidth,
+                               height: markerWidth)
+        let pos3 = CGRect.init(x: x * 7 - posRightPadding,
+                               y: bottomPadding,
+                               width: markerWidth,
+                               height: markerWidth)
+        let pos4 = CGRect.init(x: x * 9 - posRightPadding,
+                               y: bottomPadding,
+                               width: markerWidth,
+                               height: markerWidth)
+        let pos5 = CGRect.init(x: x * 12 - posRightPadding,
+                               y: bottomPadding,
+                               width: markerWidth,
+                               height: markerWidth)
+        let pos6 = CGRect.init(x: x * 15 - posRightPadding,
+                               y: bottomPadding,
+                               width: markerWidth,
+                               height: markerWidth)
+        
+        NSColor.white.set()
+        
+        let font = NSFont.boldSystemFont(ofSize: markerRect.height * 0.6)
+        let color = NSColor.white
+        let style = NSMutableParagraphStyle.init()
+        style.alignment = NSTextAlignment.center
+        let attributes = [
+            NSFontAttributeName: font,
+            NSForegroundColorAttributeName: color,
+            NSStrokeWidthAttributeName: 0.0,
+            NSParagraphStyleAttributeName: style
+            ] as [String : Any]
+        
+        // create path according to selected marker
+        switch selectedMarkers {
+        case .dots:
+            NSBezierPath.init(ovalIn: pos1).fill()
+            NSBezierPath.init(ovalIn: pos2).fill()
+            NSBezierPath.init(ovalIn: pos3).fill()
+            NSBezierPath.init(ovalIn: pos4).fill()
+            NSBezierPath.init(ovalIn: pos5).fill()
+            NSBezierPath.init(ovalIn: pos6).fill()
+        case .digits:
+            "3".draw(in: pos1, withAttributes: attributes)
+            "5".draw(in: pos2, withAttributes: attributes)
+            "7".draw(in: pos3, withAttributes: attributes)
+            "9".draw(in: pos4, withAttributes: attributes)
+            "12".draw(in: pos5, withAttributes: attributes)
+            "15".draw(in: pos6, withAttributes: attributes)
+        case .romanDigits:
+            "III".draw(in: pos1, withAttributes: attributes)
+            "V".draw(in: pos2, withAttributes: attributes)
+            "VII".draw(in: pos3, withAttributes: attributes)
+            "IX".draw(in: pos4, withAttributes: attributes)
+            "XII".draw(in: pos5, withAttributes: attributes)
+            "XV".draw(in: pos6, withAttributes: attributes)
+        case .none:
+            return
+        }
+    }
+    
     func drawNotes() {
         let fbWidth = self.bounds.width
-        let fbHeight = self.bounds.height
+        let fbHeight = self.fretboardRect.height
         
         let fretDistance = fbWidth / CGFloat(self.frets)
         var dotDiameter = (fretDistance - self.fretWidth) * 0.8
@@ -108,8 +210,9 @@ class Fretboardview: NSView {
             dotDiameter = fbHeight / (6 / 0.8) // Limit dot size
         }
         
-        let dotPosY = fbHeight / 6
-        let dotPaddingY = (dotPosY - dotDiameter) / 2 // Space between dots
+        let bottomPadding = fretboardRect.origin.y
+        let dotMaxHeight = fbHeight / 6
+        let dotPaddingY = (dotMaxHeight - dotDiameter) / 2 + bottomPadding // Space between dots
         let dotPaddingX = fretDistance - dotDiameter // Position dot on edge of a fret
         
         let allowedNotes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
@@ -122,7 +225,7 @@ class Fretboardview: NSView {
                 }
                 
                 let x = fretDistance * CGFloat(j) + dotPaddingX
-                let y = (dotPosY) * CGFloat(i) + dotPaddingY
+                let y = (dotMaxHeight) * CGFloat(i) + dotPaddingY
                 
                 let dotRect = NSRect(x: x,
                                      y: y,
@@ -139,25 +242,25 @@ class Fretboardview: NSView {
         
         note.dotColor.set()
         note.bezierPath.fill()
-        let charWidth = note.size.width / 2
-        let charHeight = note.size.height / 2
-        let font = NSFont.init(name: "Helvetica Neue", size: 13)
-        let color = NSColor.darkGray
+        let font = NSFont.systemFont(ofSize: note.size.height / 2)
+        let color = note.fontColor
         let shadow = NSShadow.init()
-        shadow.shadowOffset = CGSize.init(width: 1.0, height: 1.0)
-        shadow.shadowColor = NSColor.black
+        shadow.shadowOffset = CGSize.init(width: 1.0, height: -1.0)
+        shadow.shadowColor = NSColor.gray
         shadow.shadowBlurRadius = 1.0
         let style = NSMutableParagraphStyle.init()
         style.alignment = NSTextAlignment.center
         
-        
-        let charRect = note.size.insetBy(dx: charWidth / 2, dy: charHeight / 2)
-        note.fontColor.set()
-        note.note.draw(in: charRect, withAttributes: [
-            "NSFontAttributeName": font,
-            "NSForegroundColorAttributeName": color,
-            "NSShadowAttributeName": shadow,
-            "NSParagraphStyleAttributeName": style])
-    }
-    
+        let charX = note.size.origin.x + note.size.width / 4
+        let charY = note.size.origin.y + note.size.height / 4
+        let charPos = NSPoint(x: charX, y: charY)
+        let attributes = [
+                NSFontAttributeName: font,
+                NSForegroundColorAttributeName: color,
+                NSStrokeWidthAttributeName: 0.0,
+                NSShadowAttributeName: shadow,
+                NSParagraphStyleAttributeName: style
+            ] as [String : Any]
+        note.note.draw(at: charPos, withAttributes: attributes)
+    }    
 }
